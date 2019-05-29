e4375b38b4c8909dbe9f472085ffe8249a0344ae
Imperial Industries <33400784+Imperial-Industries@users.noreply.github.com>
2019-05-29 17:11:10 -0400

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,157 @@
 #!/bin/bash
 sudo apt-get install i3 gnome-do hsetroot docky pcmanfm
+sudo apt-get install snap
+sudo snap install code --classic
+sudo snap install vscode --classic
+sudo snap install e-tools
+sudo snap install node --channel=10/stable --classic
+sudo snap install powershell --classic
+sudo snap install webstorm --classic
+sudo snap install brackets --classic
+sudo snap install datagrip --classic
+sudo snap install clion --classic
+sudo snap install pycharm-educational --classic
+sudo snap install snapcraft --classic
+sudo snap install rubymine --classic
+sudo snap install kotlin --classic
+sudo snap install kotlin-native --classic
+sudo snap install ruby --classic
+sudo snap install ubuntu-make --classic
+sudo snap install dotnet-sdk --classic
+sudo snap install code-insiders --classic
+sudo snap install vscode-insiders --classic
+sudo snap install pycharm-community --classic
+sudo snap install notepad-plus-plus
+sudo snap install postman
+sudo snap install atom --classic
+sudo snap install phpstorm --classic
+sudo snap install android-studio --classic
+sudo snap install pycharm-professional --classic
+sudo snap install gitkraken
+sudo snap install intellij-idea-community --classic
+sudo snap install kubectl --classic
+sudo snap install cacher
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install sublime-text --classic
+sudo snap install eclipse --classic
+sudo snap install slack --classic
+sudo snap install gitter-desktop
+sudo snap install wordpress-desktop
+sudo snap install wire
+sudo snap install teams-for-linux
+sudo snap install electron-mail
+sudo snap install skype --classic
+sudo snap install rocketchat-desktop
+sudo snap install slack-term
+sudo snap install mattermost-desktop
+sudo snap install journey
+sudo snap install firefox
+sudo snap install fromscratch
+sudo snap install wavebox
+sudo snap install opera
+sudo snap install libreoffice
+sudo snap install organize-my-files
+sudo snap install ghost-desktop
+sudo snap install writefull
+sudo snap install odrive-unofficial
+sudo snap install thunderbird --edge
+sudo snap install midori
+sudo snap install taskbook
+sudo snap install emacs --classic
+sudo snap install chromium
+sudo snap install brave
+sudo snap install zenkit
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo snap install remmina
+sudo snap install termius-app
+sudo snap install mumble
+sudo snap install gnome-system-monitor
+sudo snap install htop
+sudo snap install wormhole
+sudo snap install teleconsole --classic
+sudo snap install cloudtag
+sudo snap install syncthing
+sudo snap install gotop
+sudo snap install fast
+sudo snap install bw
+sudo snap install sftpclient
+sudo snap install ubuntu-clock-app
+sudo snap install ark --candidate
+sudo snap install sensors-unity
+sudo snap install links
+sudo snap install simple-scan --classic
+sudo snap install krop
+sudo snap install shotcut --classic
+sudo snap install ffmpeg
+sudo snap install handbrake-jz
+sudo snap install screencloud
+sudo snap install opendvdproducer --edge
+sudo snap install nextcloud
+sudo snap install microk8s --classic
+sudo snap install kubelet --classic
+sudo snap install kube-proxy --classic
+sudo snap install kube-scheduler
+sudo snap install kube-controller-manager
+sudo snap install kube-apiserver
+sudo snap install mysql --edge
+sudo snap install kata-containers --classic
+sudo snap install docker
+sudo snap install juju --classic
+sudo snap install rocketchat-server
+sudo snap install canonical-livepatch
+sudo snap install google-cloud-sdk --classic
+sudo snap install lxd
+sudo snap install etcd
+sudo snap install doctl
+sudo snap install heroku --classic
+sudo snap install aws-cli --classic
+sudo snap install amazon-ssm-agent --classic
+sudo snap install bitwarden
+sudo snap install john-the-ripper
+sudo snap install ufw
+sudo snap install nitrokey-app
+sudo snap install node-red
+sudo snap install mycroft
+sudo snap install thinger-maker-server
+sudo snap install ubports-installer
+sudo snap install homebridge
+sudo snap install indicator-sensors
+sudo snap install aws-iot-greengrass
+sudo snap install modem-manager
+sudo snap install freecad
+sudo snap install blender --classic
+sudo snap install animationmaker
+sudo snap install qalculate
+sudo snap install halo-weather
+sudo snap install newsboat
+sudo snap install ubuntu-newsblur --edge
+sudo snap install wethr
+sudo snap install inkscape
+sudo snap install audacity
+sudo snap install musescore
+sudo snap install videobox
+sudo snap install ebook2cw
+sudo snap install stellarium-daily --edge
+sudo snap install sirrujak-sciencefair
+sudo snap install rigs-of-rods --candidate
+sudo snap install dataexplore
+sudo snap install openmapper-desktop
+sudo snap install iraspa
+sudo snap install kmplot
+sudo snap install yacas
+sudo snap install functy
+sudo snap install truthtables
+sudo snap install cantor
+sudo snap install step
+sudo snap install kalgebra
+sudo snap install blur
+sudo snap install adtl
+sudo snap install neuronify
+sudo snap install disk-space-saver
+sudo snap install organize-my-files
+sudo snap install snap-store
+sudo snap install easy-disk-cleaner
+sudo snap install buka
+sudo snap install opengrok
+sudo snap install polar-bookshelf
